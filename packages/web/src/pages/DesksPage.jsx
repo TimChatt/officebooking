ad952a26a25d35212d738aa4690776dca6dd9a94
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-06-20 20:41:45 +0100

    Update DesksPage.jsx

--- packages/web/src/pages/DesksPage.jsx
+++ packages/web/src/pages/DesksPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import Modal from '../components/ui/Modal.jsx';
 import Button from '../components/ui/Button.jsx';
 

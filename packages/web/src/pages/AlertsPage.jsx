e4c96ed5bcaf86ea645a7d4349dc6e1f5f9cc0d1
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-06-20 20:41:03 +0100

    Update AlertsPage.jsx

--- packages/web/src/pages/AlertsPage.jsx
+++ packages/web/src/pages/AlertsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import Button from '../components/ui/Button.jsx';
 
 export default function AlertsPage() {

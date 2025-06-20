b2d2f1338244a782679173a540f7d76af89bcd72
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-06-20 22:06:12 +0100

    Update tailwind.config.js

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,22 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#1e40af',
-        secondary: '#64748b',
-        accent: '#e11d48',
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
